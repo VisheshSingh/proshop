b8f5121e1757937a4d24a7336ff2b92088c665be
Vishesh Singh <VisheshSingh16@gmail.com>
2020-10-03 21:32:52 -0500

    Products List Reducer & Action 🏹

---
+++ frontend/src/reducers/productsReducer.js
@@ -0,0 +1,22 @@
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAILURE,
+} from '../constants/productsConstants';
+
+const initialState = {
+  products: [],
+};
+
+export const productsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, products: [] };
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, products: action.payload };
+    case PRODUCT_LIST_FAILURE:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,11 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const reducer = combineReducers({});
+import { productsReducer } from './reducers/productsReducer';
+
+const reducer = combineReducers({
+  productsList: productsReducer,
+});
 
 const initialState = {};
 
