7da509022ba0adde7e1f57e097b929ad2ea866ac
Vishesh Singh <VisheshSingh16@gmail.com>
2020-10-07 17:57:02 -0500

    User Registration & Password Encryption 🔐

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -26,6 +26,41 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Register a user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists!');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+      }),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid data provided, please check the data');
+  }
+});
+
 // @desc    Get User Profile
 // @route   GET /api/users/profile
 // @access  Private
@@ -47,5 +82,6 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 module.exports = {
   authUser,
+  registerUser,
   getUserProfile,
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { authUser, getUserProfile } = require('../controllers/userController');
+const {
+  authUser,
+  getUserProfile,
+  registerUser,
+} = require('../controllers/userController');
 const protectRoute = require('../middlewares/authMiddleware');
 
 router.post('/login', authUser);
+router.post('/', registerUser);
 router.get('/profile', protectRoute, getUserProfile);
 
 module.exports = router;
