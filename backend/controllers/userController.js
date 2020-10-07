80954cc0effce3e4e1cf60769733ca4539c09ebd
Vishesh Singh <VisheshSingh16@gmail.com>
2020-10-06 20:35:46 -0500

    User Authentication Endpoint 👨‍🦱

---
+++ backend/controllers/userController.js
@@ -0,0 +1,28 @@
+const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
+
+// @desc    Auth User & Create token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      user: user.email,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid user email or password');
+  }
+});
+
+module.exports = {
+  authUser,
+};
