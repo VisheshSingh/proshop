9ef3cc4c9c119652bcccbf6b03a14d8d644e6793
Vishesh Singh <VisheshSingh16@gmail.com>
2020-10-15 18:27:06 -0500

    📦 Order Controller & Route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 const colors = require('colors');
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 dotenv.config();
@@ -20,6 +21,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 
