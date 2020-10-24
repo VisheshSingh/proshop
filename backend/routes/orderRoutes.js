1988db96e628558346feda640ed624ae8e09e18e
Vishesh Singh <VisheshSingh16@gmail.com>
2020-10-24 13:08:08 -0500

    Update to paid 💰 endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } = require('../controllers/ordersController');
 const protectRoute = require('../middlewares/authMiddleware');
 
 router.post('/', protectRoute, addOrderItems);
 router.get('/:id', protectRoute, getOrderById);
+router.put('/:id/pay', protectRoute, updateOrderToPaid);
 
 module.exports = router;
