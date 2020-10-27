23780f9e4eec1264e8cd150b8a4ea8de2deba28f
Vishesh Singh <VisheshSingh16@gmail.com>
2020-10-26 22:09:13 -0500

    👨‍🍳User Edit Screen & Get User Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 const App = () => {
   return (
@@ -30,6 +31,7 @@ const App = () => {
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/admin/userlist' component={UserListScreen} />
+          <Route path='/admin/users/:id/edit' component={UserEditScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
