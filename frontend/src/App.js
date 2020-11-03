b15cc125f8653a7ae6540ce16a27d9d864cb91f6
Vishesh Singh <VisheshSingh16@gmail.com>
2020-11-02 19:46:11 -0600

    Paginate 🔢 HomeScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -40,7 +40,7 @@ const App = () => {
           <Route path='/admin/product/:id/edit' component={ProductEditScreen} />
           <Route path='/admin/orderlist' component={OrderListScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
-          <Route path='/search/:keyword' component={HomeScreen} />
+          <Route path='/search/:keyword' component={HomeScreen} exact />
           <Route path='/page/:pageNumber' component={HomeScreen} exact />
           <Route
             path='/search/:keyword/page/:pageNumber'
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,13 +6,14 @@ import { listProducts } from '../actions/productsAction';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 import Product from '../components/Product';
+import Paginate from '../components/Paginate';
 
 const HomeScreen = ({ match }) => {
   const keyword = match.params.keyword;
   const pageNumber = match.params.pageNumber || 1;
   const dispatch = useDispatch();
 
-  const { loading, products, error } = useSelector(
+  const { loading, products, error, pages, page } = useSelector(
     (state) => state.productsList
   );
 
@@ -28,14 +29,21 @@ const HomeScreen = ({ match }) => {
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
-        <Row>
-          {products &&
-            products.map((product) => (
-              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product} />
-              </Col>
-            ))}
-        </Row>
+        <>
+          <Row>
+            {products &&
+              products.map((product) => (
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                  <Product product={product} />
+                </Col>
+              ))}
+          </Row>
+          <Paginate
+            page={page}
+            pages={pages}
+            keyword={keyword ? keyword : ''}
+          />
+        </>
       )}
     </div>
   );
