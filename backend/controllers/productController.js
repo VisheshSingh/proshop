4642cc070720f6b031951f5d3fe56446588399a6
Vishesh Singh <VisheshSingh16@gmail.com>
2020-11-02 19:22:11 -0600

    pagination 🔢

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,8 @@ const asyncHandler = require('express-async-handler');
 // @route   GET /api/products
 // @access  Public
 const getAllProducts = asyncHandler(async (req, res) => {
+  const pageSize = 2;
+  const page = Number(req.query.pageNumber) || 1;
   const keyword = req.query.keyword
     ? {
         name: {
@@ -14,8 +16,11 @@ const getAllProducts = asyncHandler(async (req, res) => {
       }
     : {};
 
-  const products = await Product.find({ ...keyword });
-  res.json(products);
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // @desc    Fetch a single product by id
